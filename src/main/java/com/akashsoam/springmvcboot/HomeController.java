47378d4157432e1141e2a458b32b911d6a10dee6
soam1 <soamakash9@gmail.com>
2023-12-16 17:46:32 +0530

    learning @RequestParam

--- src/main/java/com/akashsoam/springmvcboot/HomeController.java
+++ src/main/java/com/akashsoam/springmvcboot/HomeController.java
@@ -2,6 +2,7 @@ package com.akashsoam.springmvcboot;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -16,11 +17,8 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/add")
-	public String add(HttpServletRequest req) {
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
+	public String add(@RequestParam("num1") int i,@RequestParam("num2") int j, HttpSession session) {
 		int num3 = i+j;
-		HttpSession session = req.getSession();
 		session.setAttribute("num3", num3);
 		return "result.jsp";
 	}
