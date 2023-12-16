6cf4fd79fcda7d8ca1bca429a7f919a194684f81
soam1 <soamakash9@gmail.com>
2023-12-16 21:47:07 +0530

    Model and ModelMap

--- src/main/java/com/akashsoam/springmvcboot/HomeController.java
+++ src/main/java/com/akashsoam/springmvcboot/HomeController.java
@@ -1,6 +1,8 @@
 package com.akashsoam.springmvcboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,11 +20,10 @@ public class HomeController {
 	}
 	
 	@RequestMapping("/add")
-	public ModelAndView add(@RequestParam("num1") int i,@RequestParam("num2") int j, HttpSession session) {
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("result");
+//	public String add(@RequestParam("num1") int i,@RequestParam("num2") int j, HttpSession session, Model m) {
+	public String add(@RequestParam("num1") int i,@RequestParam("num2") int j, HttpSession session, ModelMap m) {
 		int num3 = i+j;
-		mv.addObject("num3", num3);
-		return mv;
+		m.addAttribute("num3", num3);
+		return "result";
 	}
 }
