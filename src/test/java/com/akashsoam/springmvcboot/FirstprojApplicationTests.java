e8a4cbed624c5ef7277d0b6447b5104c7727911d
soam1 <soamakash9@gmail.com>
2023-12-16 17:37:36 +0530

    learning spring MVC

---
+++ src/test/java/com/akashsoam/springmvcboot/FirstprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akashsoam.springmvcboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
